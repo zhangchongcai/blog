028faa6eb93389d18b5dee2bbb36eaefda3b46d5
zhangchongcai <18207754940@163.com>
2020-09-08 21:28:03 +0800

    修改初始数据

--- api/apiUrl.js
+++
@@ -1,9 +0,0 @@
-const ipUrl = 'http://127.0.0.1:7001/default/'
-
-let servicePath = {
-    getArticleList : ipUrl + 'getArticleList', // 首页
-    getArticleById : ipUrl + 'getArticleById/', // 详细
-    getTypeInfo : ipUrl + 'getTypeInfo',    // 首页nav类
-    getListById : ipUrl + 'getListById/' // 列表
-}
-export default servicePath
